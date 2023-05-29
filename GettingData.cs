5f6570cbb2df1887c76aff22c66d54619cce8422
Brandon Josef Goldhawk <brandongoldhawk@gmail.com>
2023-05-29 21:56:06 +0200

    Added classes to clean the code up

---
+++ GettingData.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matric_Prelims
+{
+    internal class GettingData
+    {
+
+        public string[] GetHeadings(int desVar, int consNum)
+        {
+            string[] headingArr = new string[consNum];
+            int tempColCount = 0;
+            int tempRowCount = 0;
+            string temp;
+            for (int i = 0; i < desVar; i++) //getting heading values
+            {
+                headingArr[i] = "x" + (i + 1);
+            }
+            for (int i = desVar; i < consNum; i++)
+            {
+                Console.WriteLine("Enter sign for constraint: " + i);
+                temp = Console.ReadLine();
+                if (temp == "<=")
+                {
+                    headingArr[i] = "s" + (i + 1);
+                }
+                else if (temp == ">=")
+                {
+                    headingArr[i] = "e" + (i + 1);
+                }
+                else if (temp == "=")
+                {
+                    headingArr[i] = "e" + (i + 1);
+                    tempColCount++;
+                    tempRowCount++;
+                }
+            }
+            return headingArr;
+        }
+
+        public double[,] SetModel(int desVar, int consNum)
+        {
+            int tempColCount = 0;
+            int tempRowCount = 0;
+            desVar += tempColCount;
+            consNum += tempRowCount;
+
+            double[,] conicalArr = new double[desVar, consNum];
+
+            Console.WriteLine("What are the coeffients for the objective function?");
+            for (int i = 0; i <= desVar; i++)
+            {
+                Console.WriteLine("Enter coeffient for variable: " + i);
+                conicalArr[i - 1, 0] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("What are the coeffients for the constraints?");
+            for (int i = 1; i <= desVar; i++)
+            {
+                for (int j = 0; j < consNum; j++)
+                {
+                    Console.WriteLine("Enter coeffient for constraint variable: " + i + " " + j);
+                    conicalArr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            return conicalArr;
+        }
+
+        public string[] getXbvHeadings()
+        {
+            Console.WriteLine("Enter how many basic variables there are");
+            int numXbvValues = Convert.ToInt32(Console.ReadLine());
+            string[] xbvValue = new string[numXbvValues];
+
+            for (int i = 0; i < numXbvValues; i++)
+            {
+                Console.WriteLine("Enter the heading of the Basic Variable");
+                xbvValue[i] = Console.ReadLine();
+            }
+
+            return xbvValue;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿using System.Security.Cryptography;
+﻿using Matric_Prelims;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         //need to do error handling when entering data
-        
-        GettingDate getData = new GettingDate();
+
+        GettingData getData = new GettingData();
+        MatrixMaths matrixMaths = new MatrixMaths();
 
         Console.WriteLine("Enter Matrix Size");
         int row=0;
@@ -29,15 +30,15 @@ internal class Program
         DisplayMatrix(matrix);
         
         int n = matrix.GetLength(0);
-        double det = GetDeterminant(matrix, n);
+        double det = matrixMaths.GetDeterminant(matrix, n);
         Console.WriteLine("Determinant: " + det);
         
-        double[,] cofactorMatrix = GetCofactorMatrix(matrix, n);
+        double[,] cofactorMatrix = matrixMaths.GetCofactorMatrix(matrix, n);
         Console.WriteLine("Cofactor Matrix:");
         DisplayMatrix(cofactorMatrix);
         
         Console.WriteLine("Transposed Matrix:");
-        double[,] transposedMatrix = TransposeMatrix(cofactorMatrix);
+        double[,] transposedMatrix = matrixMaths.TransposeMatrix(cofactorMatrix);
         DisplayMatrix(transposedMatrix);
 
         if (det == 0)
@@ -46,7 +47,7 @@ internal class Program
         }
         else
         {
-            double[,] inverseMatrix = GetInverseMatrix(transposedMatrix, det);
+            double[,] inverseMatrix = matrixMaths.GetInverseMatrix(transposedMatrix, det);
             Console.WriteLine("Inverse Matrix:");
             DisplayMatrix(inverseMatrix);
         }
@@ -57,11 +58,12 @@ internal class Program
         int consNum = Convert.ToInt32(Console.ReadLine()) * 2; //row num times by two for s and e var
         
         //original lp
-        double[,] modelArr = SetModel(desVar, consNum);
-        string[] headings = GetHeadings(desVar, consNum);
+        double[,] modelArr = getData.SetModel(desVar, consNum);
+        string[] headings = getData.GetHeadings(desVar, consNum);
+        
 
         //solved lp
-        double[,] solvedArr = SetModel(desVar, consNum);
+        double[,] solvedArr = getData.SetModel(desVar, consNum);
 
         //Entering XBv value headings
         string[] xbv = getXbvHeadings();
@@ -101,167 +103,6 @@ internal class Program
             }
             Console.WriteLine();
         }
-    }
-
-    private static double[,] GetInverseMatrix(double[,] matrix, double determinant)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        double[,] inverseMatrix = new double[rows, cols];
-        double det = 1 / determinant;
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                inverseMatrix[i, j] = det * matrix[i, j];
-            }
-        }
-
-        return inverseMatrix;
-    }
-    
-    
-    private static double GetDeterminant(double[,] matrix, int k)
-    {
-        if (k == 1)
-        {
-            return matrix[0, 0];
-        }
-        else if (k == 2)
-        {
-            return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
-        }
-        else
-        {
-            double det = 0;
-            for (int i = 0; i < k; i++)
-            {
-                double[,] submatrix = GetSubmatrix(matrix, 0, i, k);
-                double minor = GetDeterminant(submatrix, k - 1);
-                double cofactor = ((i % 2) == 0) ? minor : -minor;
-                det += matrix[0, i] * cofactor;
-            }
-            return det;
-        }
-    }
-
-    private static double[,] GetSubmatrix(double[,] matrix, int rowToRemove, int colToRemove, int n)
-    {
-        double[,] submatrix = new double[n - 1, n - 1];
-        int rowIndex = 0;
-        int colIndex = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (i == rowToRemove)
-            {
-                continue;
-            }
-            colIndex = 0;
-            for (int j = 0; j < n; j++)
-            {
-                if (j == colToRemove)
-                {
-                    continue;
-                }
-                submatrix[rowIndex, colIndex] = matrix[i, j];
-                colIndex++;
-            }
-            rowIndex++;
-        }
-        return submatrix;
-    }
-
-    private static double[,] GetCofactorMatrix(double[,] matrix, int n)
-    {
-        double[,] cofactorMatrix = new double[n, n];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                double[,] submatrix = GetSubmatrix(matrix, i, j, n);
-                double minor = GetDeterminant(submatrix, n - 1);
-                double cofactor = ((i + j) % 2 == 0) ? minor : -minor;
-                cofactorMatrix[i, j] = cofactor;
-            }
-        }
-        return cofactorMatrix;
-    }
-
-    private static double[,] TransposeMatrix(double[,] matrix)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        double[,] transposeMatrix = new double[cols, rows];
-        for (int i = 0; i < cols; i++)
-        {
-            for (int j = 0; j < rows; j++)
-            {
-                transposeMatrix[i, j] = matrix[j, i];
-            }
-        }
-        return transposeMatrix;
-    }
-
-    private static string[] GetHeadings(int desVar, int consNum)
-    {
-        string[] headingArr = new string[consNum];
-        int tempColCount = 0;
-        int tempRowCount = 0;
-        string temp;
-        for (int i = 0; i < desVar; i++) //getting heading values
-        {
-            headingArr[i] = "x" + (i + 1);
-        }
-        for (int i = desVar; i < consNum; i++)
-        {
-            Console.WriteLine("Enter sign for constraint: " + i);
-            temp = Console.ReadLine();
-            if (temp == "<=")
-            {
-                headingArr[i] = "s" + (i + 1);
-            }
-            else if (temp == ">=")
-            {
-                headingArr[i] = "e" + (i + 1);
-            }
-            else if (temp == "=")
-            {
-                headingArr[i] = "e" + (i + 1);
-                tempColCount++;
-                tempRowCount++;
-            }
-        }
-        return headingArr;
-    }
-
-    private static double[,] SetModel(int desVar, int consNum)
-    {
-        int tempColCount = 0;
-        int tempRowCount = 0;
-        desVar += tempColCount;
-        consNum += tempRowCount;
-
-        double[,] conicalArr = new double[desVar, consNum];
-
-        Console.WriteLine("What are the coeffients for the objective function?");
-        for (int i = 0; i <= desVar; i++)
-        {
-            Console.WriteLine("Enter coeffient for variable: " + i);
-            conicalArr[i - 1, 0] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        Console.WriteLine("What are the coeffients for the constraints?");
-        for (int i = 1; i <= desVar; i++)
-        {
-            for (int j = 0; j < consNum; j++)
-            {
-                Console.WriteLine("Enter coeffient for constraint variable: " + i + " " + j);
-                conicalArr[i, j] = Convert.ToInt32(Console.ReadLine());
-            }
-        }
-
-        return conicalArr;
     }
 
     public static string [] getXbvHeadings()
